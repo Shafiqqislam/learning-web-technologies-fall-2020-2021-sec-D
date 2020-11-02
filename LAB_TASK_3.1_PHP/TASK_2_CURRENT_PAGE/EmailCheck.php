d95af612500399cd18354231ff2018a013ed4f26
Shafiqqislam <Shafiqislam7901@gmail.com>
2020-11-02 12:55:23 +0600

    task_2_b

---
+++ LAB_TASK_3.1_PHP/TASK_2_CURRENT_PAGE/EmailCheck.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_REQUEST['submit'])){
+    $name =$_REQUEST['name'];
+    if($name==""){
+        echo "field required...";
+    }else{
+            echo=$name;
+        
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form style="width:300px;" method="POST" action="#">
+                <fieldset>
+            <legend>EMAIL</legend>
+            <input type="email" name="email" required="required" >
+            <button title="hint:sample@example.com"><b>i</b></button><br>
+            <hr style="width:300px;" align="left";>
+            <input type="submit" name="submit" value="submit">
+
+        </fieldset>
+
+    </form>
+</body>
+</html>
